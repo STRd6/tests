c67c9c92c9a16cd3e000075b5ec2e1c799fabf34
Daniel X Moore <yahivin@gmail.com>
2013-09-12 14:27:34 -0700

    Built master in browser in strd6.github.io/editor

--- master.js
+++ master.js
@@ -1,7 +1,7 @@
 (function (ENV) {
 (function() {
   this.TestRunner = {
-    launch: function(testCode, options) {
+    launch: function(testScripts, options) {
       var config, html, sandbox;
       if (options == null) {
         options = {};
@@ -10,7 +10,7 @@
         width: 800,
         height: 600
       });
-      html = "<html>\n<head>\n  <meta charset=\"utf-8\">\n  <title>Mocha Tests</title>\n  <link rel=\"stylesheet\" href=\"http://strd6.github.io/tests/mocha.css\" />\n</head>\n<body>\n  <div id=\"mocha\"></div>\n  <script src=\"http://strd6.github.io/tests/assert.js\"><\/script>\n  <script src=\"http://strd6.github.io/tests/mocha.js\"><\/script>\n  <script>mocha.setup('bdd')<\/script>\n  <script>" + testCode + "<\/script>\n  <script>\n    mocha.checkLeaks();\n    mocha.globals(['jQuery']);\n    mocha.run();\n  <\/script>\n</body>\n</html>";
+      html = "<html>\n<head>\n  <meta charset=\"utf-8\">\n  <title>Mocha Tests</title>\n  <link rel=\"stylesheet\" href=\"http://strd6.github.io/tests/mocha.css\" />\n</head>\n<body>\n  <div id=\"mocha\"></div>\n  <script src=\"http://strd6.github.io/tests/assert.js\"><\/script>\n  <script src=\"http://strd6.github.io/tests/mocha.js\"><\/script>\n  <script>mocha.setup('bdd')<\/script>\n  " + testScripts + "\n  <script>\n    mocha.checkLeaks();\n    mocha.globals(['jQuery']);\n    mocha.run();\n  <\/script>\n</body>\n</html>";
       sandbox = Sandbox({
         width: config.width,
         height: config.height
@@ -30,23 +30,23 @@
       "content": "tests\n=====\n\nProvide tests to online editor\n",
       "type": "blob"
     },
-    "source/test.coffee.md": {
-      "path": "source/test.coffee.md",
-      "mode": "100644",
-      "content": "Export `TestRunner` object.\n\n    @TestRunner = \n\nRun tests in a sandboxed window\n\n      launch: (testCode, options={}) ->\n        config = Object.reverseMerge {}, options,\n          width: 800\n          height: 600\n      \n        html = \"\"\"\n          <html>\n          <head>\n            <meta charset=\"utf-8\">\n            <title>Mocha Tests</title>\n            <link rel=\"stylesheet\" href=\"http://strd6.github.io/tests/mocha.css\" />\n          </head>\n          <body>\n            <div id=\"mocha\"></div>\n            <script src=\"http://strd6.github.io/tests/assert.js\"><\\/script>\n            <script src=\"http://strd6.github.io/tests/mocha.js\"><\\/script>\n            <script>mocha.setup('bdd')<\\/script>\n            <script>#{testCode}<\\/script>\n            <script>\n              mocha.checkLeaks();\n              mocha.globals(['jQuery']);\n              mocha.run();\n            <\\/script>\n          </body>\n          </html>\n        \"\"\"\n  \n        sandbox = Sandbox\n          width: config.width\n          height: config.height\n        \n        sandbox.document.open()\n        sandbox.document.write(html)\n        sandbox.document.close()\n        ",
-      "type": "blob"
-    },
     "TODO": {
       "path": "TODO",
       "mode": "100644",
       "content": "Investigate Leak Checking\n",
+      "type": "blob"
+    },
+    "source/test.coffee.md": {
+      "path": "source/test.coffee.md",
+      "mode": "100644",
+      "content": "Export `TestRunner` object.\n\n    @TestRunner = \n\nRun tests in a sandboxed window\n\n      launch: (testScripts, options={}) ->\n        config = Object.reverseMerge {}, options,\n          width: 800\n          height: 600\n      \n        html = \"\"\"\n          <html>\n          <head>\n            <meta charset=\"utf-8\">\n            <title>Mocha Tests</title>\n            <link rel=\"stylesheet\" href=\"http://strd6.github.io/tests/mocha.css\" />\n          </head>\n          <body>\n            <div id=\"mocha\"></div>\n            <script src=\"http://strd6.github.io/tests/assert.js\"><\\/script>\n            <script src=\"http://strd6.github.io/tests/mocha.js\"><\\/script>\n            <script>mocha.setup('bdd')<\\/script>\n            #{testScripts}\n            <script>\n              mocha.checkLeaks();\n              mocha.globals(['jQuery']);\n              mocha.run();\n            <\\/script>\n          </body>\n          </html>\n        \"\"\"\n  \n        sandbox = Sandbox\n          width: config.width\n          height: config.height\n        \n        sandbox.document.open()\n        sandbox.document.write(html)\n        sandbox.document.close()\n        ",
       "type": "blob"
     }
   },
   "distribution": {
     "build.js": {
       "path": "build.js",
-      "content": "(function() {\n  this.TestRunner = {\n    launch: function(testCode, options) {\n      var config, html, sandbox;\n      if (options == null) {\n        options = {};\n      }\n      config = Object.reverseMerge({}, options, {\n        width: 800,\n        height: 600\n      });\n      html = \"<html>\\n<head>\\n  <meta charset=\\\"utf-8\\\">\\n  <title>Mocha Tests</title>\\n  <link rel=\\\"stylesheet\\\" href=\\\"http://strd6.github.io/tests/mocha.css\\\" />\\n</head>\\n<body>\\n  <div id=\\\"mocha\\\"></div>\\n  <script src=\\\"http://strd6.github.io/tests/assert.js\\\"><\\/script>\\n  <script src=\\\"http://strd6.github.io/tests/mocha.js\\\"><\\/script>\\n  <script>mocha.setup('bdd')<\\/script>\\n  <script>\" + testCode + \"<\\/script>\\n  <script>\\n    mocha.checkLeaks();\\n    mocha.globals(['jQuery']);\\n    mocha.run();\\n  <\\/script>\\n</body>\\n</html>\";\n      sandbox = Sandbox({\n        width: config.width,\n        height: config.height\n      });\n      sandbox.document.open();\n      sandbox.document.write(html);\n      return sandbox.document.close();\n    }\n  };\n\n}).call(this);",
+      "content": "(function() {\n  this.TestRunner = {\n    launch: function(testScripts, options) {\n      var config, html, sandbox;\n      if (options == null) {\n        options = {};\n      }\n      config = Object.reverseMerge({}, options, {\n        width: 800,\n        height: 600\n      });\n      html = \"<html>\\n<head>\\n  <meta charset=\\\"utf-8\\\">\\n  <title>Mocha Tests</title>\\n  <link rel=\\\"stylesheet\\\" href=\\\"http://strd6.github.io/tests/mocha.css\\\" />\\n</head>\\n<body>\\n  <div id=\\\"mocha\\\"></div>\\n  <script src=\\\"http://strd6.github.io/tests/assert.js\\\"><\\/script>\\n  <script src=\\\"http://strd6.github.io/tests/mocha.js\\\"><\\/script>\\n  <script>mocha.setup('bdd')<\\/script>\\n  \" + testScripts + \"\\n  <script>\\n    mocha.checkLeaks();\\n    mocha.globals(['jQuery']);\\n    mocha.run();\\n  <\\/script>\\n</body>\\n</html>\";\n      sandbox = Sandbox({\n        width: config.width,\n        height: config.height\n      });\n      sandbox.document.open();\n      sandbox.document.write(html);\n      return sandbox.document.close();\n    }\n  };\n\n}).call(this);",
       "type": "blob"
     }
   },
